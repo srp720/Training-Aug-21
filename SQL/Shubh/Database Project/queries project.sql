c88db6e85e8239cb41d722bd4b49fd6a9329ae62
Shubh Patel <pshubh333@gmail.com>
2021-08-30 19:54:01 +0530

    REFS #704, #705, #706, #707, #708 FIXES ISSUES: PREPARED QUERIES

--- SQL/Shubh/Database Project/queries project.sql
+++ SQL/Shubh/Database Project/queries project.sql
@@ -14,7 +14,12 @@ SELECT CL.ClusterLocationId, CL.Locations, C.CityName AS 'City' FROM Cities C JO
 
 
 --5.Charity Events in a City
-SELECT CE.EventOrganiserId, CE.EventName, C.CityName FROM Cities C JOIN CharityEvent CE ON C.CityId=CE.CityId
+SELECT CE.EventId, O.OrganisationLogoURL, O.OrganisationName, UD.UserName, CE.EventDescription, C.CityName FROM CharityEvent CE
+JOIN UserData UD ON CE.EventOrganiserId = UD.UserId
+JOIN Organisation O ON CE.EventOrganiserId=O.OrganisationUserId
+JOIN Cities C ON CE.CityId = C.CityId
+WHERE C.CityId = 2  AND CE.IsCompleted = 0
+ORDER BY CE.CreatedAt DESC
 
 
 --6.User Profile
@@ -29,51 +34,29 @@ ProfileImage='https://media-exp1.licdn.com/dms/image/C4E0BAQHj-tQ4PuqAsg/company
 EmailAddress='parmarhemanshu733@gmail.com',
 MobileNo= 1043355325
 WHERE UserId = 2
-    
+   
+   
 --8.Post Feed
-(
-SELECT P.PostId, UD.ProfileImage, UD.UserName, P.LocationName, P.ImageURL, CAST(P.Description AS NVARCHAR) AS 'Post Description', P.HelpRequiredCount, 
+WITH POSTU AS (
+SELECT P.PostId, UD.ProfileImage, UD.UserName, P.LocationName, P.ImageURL, CAST(P.Description AS NVARCHAR) AS 'PostDescription', P.HelpRequiredCount, P.PincodeId, P.CityId, P.StateId, P.IsClosed,
 CASE WHEN UC.UrgencyCount IS NULL THEN 0 ELSE UC.UrgencyCount END AS 'UrgencyCount', 
 CASE WHEN SC.SpamCount IS NULL THEN 0 ELSE SC.SpamCount END AS 'SpamCount'
 FROM Post P 
 JOIN UserData UD ON P.UserId = UD.UserId
 LEFT JOIN (SELECT PostId, COUNT(UserId) AS 'UrgencyCount' FROM Urgency GROUP BY PostId) UC ON P.PostId = UC.PostId
 LEFT JOIN (SELECT PostId, COUNT(SpamId) AS 'SpamCount' FROM Spam GROUP BY PostId) SC ON P.PostId = SC.PostId
-WHERE P.PincodeId = 6 AND P.IsClosed = 0--@User'sPincodeId
 )
+SELECT PostId, ProfileImage, UserName, LocationName, ImageURL, PostDescription, HelpRequiredCount, UrgencyCount, SpamCount FROM POSTU
+WHERE PincodeId = 6 AND IsClosed = 0--@User'sPincodeId
 UNION
-(
-SELECT P.PostId, UD.ProfileImage, UD.UserName, P.LocationName, P.ImageURL, CAST(P.Description AS NVARCHAR) AS 'Post Description', P.HelpRequiredCount, 
-CASE WHEN UC.UrgencyCount IS NULL THEN 0 ELSE UC.UrgencyCount END AS 'UrgencyCount', 
-CASE WHEN SC.SpamCount IS NULL THEN 0 ELSE SC.SpamCount END AS 'SpamCount'
-FROM Post P 
-JOIN UserData UD ON P.UserId = UD.UserId
-LEFT JOIN (SELECT PostId, COUNT(UserId) AS 'UrgencyCount' FROM Urgency GROUP BY PostId) UC ON P.PostId = UC.PostId
-LEFT JOIN (SELECT PostId, COUNT(SpamId) AS 'SpamCount' FROM Spam GROUP BY PostId) SC ON P.PostId = SC.PostId
-WHERE P.CityId = 1 AND P.IsClosed = 0--@User'sCityId
-)
+SELECT PostId, ProfileImage, UserName, LocationName, ImageURL, PostDescription, HelpRequiredCount, UrgencyCount, SpamCount FROM POSTU
+WHERE CityId = 1 AND IsClosed = 0--@User'sCityId
 UNION
-(
-SELECT P.PostId, UD.ProfileImage, UD.UserName, P.LocationName, P.ImageURL, CAST(P.Description AS NVARCHAR) AS 'Post Description', P.HelpRequiredCount, 
-CASE WHEN UC.UrgencyCount IS NULL THEN 0 ELSE UC.UrgencyCount END AS 'UrgencyCount', 
-CASE WHEN SC.SpamCount IS NULL THEN 0 ELSE SC.SpamCount END AS 'SpamCount'
-FROM Post P 
-JOIN UserData UD ON P.UserId = UD.UserId
-LEFT JOIN (SELECT PostId, COUNT(UserId) AS 'UrgencyCount' FROM Urgency GROUP BY PostId) UC ON P.PostId = UC.PostId
-LEFT JOIN (SELECT PostId, COUNT(SpamId) AS 'SpamCount' FROM Spam GROUP BY PostId) SC ON P.PostId = SC.PostId
-WHERE P.StateId = 1 AND P.IsClosed = 0--@User'sStateId
-)
+SELECT PostId, ProfileImage, UserName, LocationName, ImageURL, PostDescription, HelpRequiredCount, UrgencyCount, SpamCount FROM POSTU
+WHERE StateId = 1 AND IsClosed = 0--@User'sStateId
 UNION
-(
-SELECT P.PostId, UD.ProfileImage, UD.UserName, P.LocationName, P.ImageURL, CAST(P.Description AS NVARCHAR) AS 'Post Description', P.HelpRequiredCount, 
-CASE WHEN UC.UrgencyCount IS NULL THEN 0 ELSE UC.UrgencyCount END AS 'UrgencyCount', 
-CASE WHEN SC.SpamCount IS NULL THEN 0 ELSE SC.SpamCount END AS 'SpamCount'
-FROM Post P 
-JOIN UserData UD ON P.UserId = UD.UserId
-LEFT JOIN (SELECT PostId, COUNT(UserId) AS 'UrgencyCount' FROM Urgency GROUP BY PostId) UC ON P.PostId = UC.PostId
-LEFT JOIN (SELECT PostId, COUNT(SpamId) AS 'SpamCount' FROM Spam GROUP BY PostId) SC ON P.PostId = SC.PostId
-WHERE P.IsClosed = 0
-)
+SELECT PostId, ProfileImage, UserName, LocationName, ImageURL, PostDescription, HelpRequiredCount, UrgencyCount, SpamCount FROM POSTU
+WHERE IsClosed = 0
 
 
 --9.Fetch Urgency Count
@@ -89,22 +72,20 @@ GROUP BY PostId
 
 
 --11.Fetch Longitude, Latitude to Google Maps API
-SELECT Latitude, Longitude FROM Post WHERE PostId IN  ('1','2','3')
+SELECT Latitude, Longitude FROM Post WHERE PostId IN (1,2,3)
 
 
-
---12.Post on Location Search 
-SELECT P.PostId, UD.ProfileImage, UD.UserName, P.LocationName, P.ImageURL, P.Description, P.HelpRequiredCount, 
-CASE WHEN UC.UrgencyCount IS NULL THEN 0 ELSE UC.UrgencyCount END AS 'UrgencyCount', 
+--12.Post on Location Searchï¿½
+SELECT P.PostId, UD.ProfileImage, UD.UserName, P.LocationName, P.ImageURL, P.Description, P.HelpRequiredCount,ï¿½
+CASE WHEN UC.UrgencyCount IS NULL THEN 0 ELSE UC.UrgencyCount END AS 'UrgencyCount',ï¿½
 CASE WHEN SC.SpamCount IS NULL THEN 0 ELSE SC.SpamCount END AS 'SpamCount'
 FROM Post P
 JOIN UserData UD ON P.UserId = UD.UserId
 LEFT JOIN (SELECT PostId, COUNT(UserId) AS 'UrgencyCount' FROM Urgency GROUP BY PostId) UC ON P.PostId = UC.PostId
 LEFT JOIN (SELECT PostId, COUNT(SpamId) AS 'SpamCount' FROM Spam GROUP BY PostId) SC ON P.PostId = SC.PostId
 JOIN Cities C ON P.CityId = C.CityId
-WHERE C.CityName = 'Ahmedabad' AND P.IsClosed = 0
+WHERE C.CityId = 1 AND P.IsClosed = 0
 ORDER BY P.CreatedAt DESC
-
 
 
 --13.Post on Need Type
@@ -130,16 +111,16 @@ SELECT CONVERT(VARCHAR(8), DATEADD(SECOND, DATEDIFF(SECOND,@StartTime, @EndTime)
 
 
 --16.City wise Organisation names
-SELECT  O.OrganisationName
+SELECT  O.OrganisationName, C.CityName
 FROM Organisation O JOIN UserData UD ON O.OrganisationUserId = UD.UserId
 JOIN Cities C ON UD.CityId = C.CityId
-WHERE C.CityName = 'Vadodara'
+WHERE C.CityId = 6
 
 
 --17.Search Organisation by username
 SELECT O.OrganisationName 
 FROM UserData UD JOIN Organisation O ON UD.UserId = O.OrganisationUserId 
-WHERE UD.UserName = 'Srp720'
+WHERE UD.UserName LIKE 'Srp720'
 
 
 --18.SELECT POST WHERE REQUIREMENT TYPE IS FOOD/SHELTER/EDUCATION
@@ -183,10 +164,57 @@ SELECT UserId, CONCAT(FirstName,' ',LastName) AS Name FROM UserData WHERE IsDail
 
 
 --25.SELECT POSTS WHICH ARE CLOSED TO MARK THEM AS COMPLETED
-SELECT PostId, ImageURL FROM POST WHERE IsClosed=1
+SELECT PostId, ImageURL FROM POST WHERE IsClosed=1 
 
 
 --26.DESCRIBE DIFFERENT ROLES OF USERS
 SELECT UD.UserId, UD.UserName, CONCAT(UD.FirstName,' ',UD.LastName) AS Name, RT.RoleType AS Role FROM UserData UD 
 LEFT JOIN Roles R ON UD.UserId=R.UserId 
 JOIN RoleType RT ON R.RoleTypeId=RT.RoleTypeId
+
+                                  
+--27.YEARLY REPORT OF POST
+;WITH ClosedNeeds
+AS (
+SELECT YEAR(CreatedAt) AS 'Year', C.CityName, COUNT(P.PostId) AS 'ClosedPost'
+FROM Post P
+JOIN Cities C ON P.CityId = C.CityId
+WHERE P.IsClosed = 1
+GROUP BY YEAR(CreatedAt), C.CityName
+)
+, OpenNeeds
+AS (
+SELECT YEAR(CreatedAt) AS 'Year', C.CityName, COUNT(P.PostId) AS 'OpenPost'
+FROM Post P
+JOIN Cities C ON P.CityId = C.CityId
+WHERE P.IsClosed = 0
+GROUP BY YEAR(CreatedAt), C.CityName
+)
+SELECT YEAR(P.CreatedAt) AS 'Year', C.CityName, COUNT(P.PostId) AS 'TotalPost', ISNULL(O.OpenPost,0) AS OpenPost, ISNULL(CN.ClosedPost,0)ï¿½AS ClosedPost
+FROM Post P
+JOIN Cities C ON P.CityId = C.CityId
+LEFT JOIN OpenNeeds O ON YEAR(P.CreatedAt) = O.Year AND (C.CityName = O.CityName)ï¿½
+LEFT JOIN ClosedNeeds CN ON YEAR(P.CreatedAt) = CN.Year AND (C.CityName = CN.CityName)
+GROUP BY YEAR(P.CreatedAt), C.CityName, O.OpenPost, CN.ClosedPost
+
+
+-------------------------------------------------"OR"-------------------------------------------------
+
+
+SELECT ISNULL(YEAR(P.CreatedAt),1947) AS Year, C.CityName, COUNT(P.PostId) AS 'TotalPost',
+    CASE
+00    WHEN P.IsClosed=0 THEN 'Requirements needs to be fulfilled'
+    WHEN P.IsClosed=1 THEN 'Requirements fulfilled'
+	ELSE 'Have not posted yet'
+END AS Status
+FROM Post P FULL JOIN Cities C ON P.CityId=C.CityId 
+GROUP BY C.CityName, P.IsClosed, YEAR(P.CreatedAt)
+
+
+--28. POST REPORT YEARLY REPORT
+SELECT C.CityName, YEAR(P.CreatedAt) AS Year, PC.Pincode, P.PostId, UD.ProfileImage, UD.UserName, P.LocationName, P.ImageURL, P.Description, P.HelpRequiredCount, P.IsClosed
+FROM Post P
+JOIN UserData UD ON P.UserId = UD.UserId
+LEFT JOIN Pincode PC ON P.PincodeId=PC.PincodeId
+JOIN Cities C ON P.CityId = C.CityId
+ORDER BY P.CreatedAt ASC
